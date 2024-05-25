c9b87e2a52f4f4f3b2ea35453d1de72ada0ba034
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-05-25 22:05:17 +0300

    infrastructure setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,71 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-yet';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { GraphQLModule } from '@nestjs/graphql';
+// import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+
+        APP_PORT: Joi.string().required(),
+
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().integer().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+
+        REDIS_PASS: Joi.string().required(),
+        REDIS_URI: Joi.string().required(),
+
+        MAIL_TRANSPORT: Joi.string().required(),
+        MAIL_HOST: Joi.string().required(),
+        MAIL_USER: Joi.string().required(),
+        MAIL_PASS: Joi.string().required(),
+
+        YANDEX_ACCES: Joi.string().required(),
+        YANDEX_SECRET: Joi.string().required(),
+        YANDEX_BUCKET: Joi.string().required(),
+      }),
+    }),
+    EventEmitterModule.forRoot(),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        const store = await redisStore({
+          ttl: 3 * 60 * 1000,
+          url: config.get('REDIS_URI'),
+        });
+        return { store };
+      },
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        port: config.get('POSTGRES_PORT'),
+        database: config.get('POSTGRES_DB'),
+        username: config.get('POSTGRES_USER'),
+        password: config.get('POSTGRES_PASSWORD'),
+        host: config.get('POSTGRES_HOST'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   autoSchemaFile: 'src/schema.gql',
+    // }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
+import helmet from 'helmet';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors) => {
+        return new BadRequestException({
+          message: errors[0].constraints[Object.keys(errors[0].constraints)[0]],
+        });
+      },
+      stopAtFirstError: true,
+    }),
+  );
+
+  const corsOptions = {
+    origin: config.get('CLIENT_URL'),
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
+  app.use(helmet());
+
+  app.use(cookieParser());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('IRecommned')
+    .setDescription('Api description for irecommend app')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document);
+  await app.listen(config.get('APP_PORT'));
 }
 bootstrap();
