7081b63354ddc6b1aac80374783981f02185ae0d
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-07-12 18:47:26 +0300

    save

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  sendLink() {
-    return `<div>Go to <a href='https://irecommend.vercel.app'>frontend</a></div>`;
+  hello() {
+    return 'Hello world!';
   }
 }
