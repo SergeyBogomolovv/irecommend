e96d89009ef17e61144f2140b1d20f4a96ecfc68
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-10-06 22:55:51 +0300

    chore: update recommendations pagination limit

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -2,4 +2,4 @@ export const REFRESH_TOKEN_KEY = 'refresh_token';
 export const USER_REQUEST_KEY = 'user';
 export const VERIFY_ACCOUNT_KEY = 'verify-account-otp';
 export const RESET_PASSWORD_KEY = 'reset-password-otp';
-export const RECOMMENDATIONS_LIMIT = 5;
+export const RECOMMENDATIONS_LIMIT = 15;
